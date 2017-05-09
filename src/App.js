1a62e628e7d79568d5cb1ee66be18cabd119275f
Marko Haanranta <marko.haanranta@helsinki.fi>
2017-05-09 11:40:53 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './App.css'
+
+class App extends Component {
+  render () {
+    return (
+      <div className='App'>
+        <p>Hello green-winter</p>
+      </div>
+    )
+  }
+}
+
+export default App
